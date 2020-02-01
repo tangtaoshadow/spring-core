2f920c58fb9079d11b3aada13924457d48e9a584
tangtao <tangtao2099@outlook.com>
2020-02-01 22:50:52 +0800

    tangtao https://www.promiselee.cn/tao modify: 添加 Student.class updateTime: 2020-2-1 22:50:41

---
+++ src/main/java/com/tao/pojo/Student.java
@@ -0,0 +1,13 @@
+package com.tao.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+public class Student {
+  private String name;
+  private Demo demo;
+}
