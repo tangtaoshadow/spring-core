d7d846af051778eab388e1f1fb30e54b610c93e9
tangtao <tangtao2099@outlook.com>
2020-02-01 20:09:28 +0800

    tangtao https://www.promiselee.cn/tao modify: 增加log4j updateTime: 2020-2-1 20:08:54

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,15 +1,22 @@
 import com.tao.pojo.Demo;
+import java.util.logging.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyTest {
+  private static Logger log = Logger.getLogger(String.valueOf(MyTest.class));
+
   public static void main(String[] args) {
     // 获取Spring的上下文对象
     ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
     // 获取spring管理的bean 对象
     Demo demo = (Demo) applicationContext.getBean("beanDemo");
-    System.out.println(demo.toString());
+    log.info("获取beanDemo " + demo.toString());
     demo = (Demo) applicationContext.getBean("beanDemo2");
-    System.out.println(demo.toString());
+    log.info("获取beanDemo2 " + demo.toString());
+
+    // 通过别名获取bean
+    demo = (Demo) applicationContext.getBean("demo2020");
+    log.info("通过别名获取bean " + demo.toString());
   }
 }
