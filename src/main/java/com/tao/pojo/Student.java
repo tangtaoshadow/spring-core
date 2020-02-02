ced26f042f98fd395c751e9a1776c0e4184c6be5
tangtao <tangtao2099@outlook.com>
2020-02-02 21:52:34 +0800

    tangtao https://www.promiselee.cn/tao modify: 启用注解 自动装配 updateTime: 2020-2-2 21:52:22

--- src/main/java/com/tao/pojo/Student.java
+++ src/main/java/com/tao/pojo/Student.java
@@ -3,11 +3,12 @@ package com.tao.pojo;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @ToString
 @Getter
 @Setter
 public class Student {
   private String name;
-  private Demo demo;
+  @Autowired private Demo demo;
 }
--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,5 +1,3 @@
-import com.tao.pojo.Demo;
-import com.tao.pojo.Person;
 import com.tao.pojo.Student;
 import java.util.logging.Logger;
 import org.junit.Test;
@@ -9,92 +7,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MyTest {
   private static Logger log = Logger.getLogger(String.valueOf(MyTest.class));
 
-  public static void main(String[] args) {
-    // 获取Spring的上下文对象
-    ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-    // 获取spring管理的bean 对象
-    Demo demo = (Demo) applicationContext.getBean("beanDemo");
-    log.info("获取beanDemo " + demo.toString());
-    demo = (Demo) applicationContext.getBean("beanDemo2");
-    log.info("获取beanDemo2 " + demo.toString());
-
-    // 通过name获取bean
-    demo = (Demo) applicationContext.getBean("demo2020");
-    log.info("通过name获取bean " + demo.toString());
-    // 通过alias获取bean
-    demo = (Demo) applicationContext.getBean("aliasBeanDemo2");
-    log.info("通过alias获取bean " + demo.toString());
-
-    // 通过beans2.xml获取bean
-    demo = (Demo) applicationContext.getBean("aliasBeans2Demo");
-    log.info("通过beans2.xml获取bean " + demo.toString());
-
-    // 获取 person
-    Person person = (Person) applicationContext.getBean("person1");
-    log.info("通过id获取 Person " + person.toString());
-    /*
-    Person(
-      name=tangtao,
-      age=22,
-      strings=[string1, string2, string3],
-      demo=Demo(name=beanDemo 2, status=100),
-      list=[this is list 1 ., this is list 2 ., this is list 3 .],
-      set=[set 1, set 2, set 3],
-      map={key1=map1, key2=map2, key3=map3},
-      properties={prop2=properties 2, prop1=properties 1, prop3=properties 3}
-    )
-     */
-
-    // 获取 person
-    demo = (Demo) applicationContext.getBean("demo2020");
-    log.info("通过name获取bean " + demo.toString());
-  }
-
   @Test
   public void test() {
     // 获取Spring的上下文对象
     ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
     // 获取 p-namespace 注入的 bean 对象
-    Demo demo = applicationContext.getBean("beanDemo3", Demo.class);
-    log.info("获取 p-namespace 注入的 bean 对象 beanDemo3 " + demo.toString());
-
-    // 获取 c-namespace 注入的 bean 对象
-    demo = applicationContext.getBean("beanDemo4", Demo.class);
-    log.info("获取 c-namespace 注入的 bean 对象 beanDemo4 " + demo.toString());
-  }
-
-  //  测试scope
-  @Test
-  public void testScope() {
-    // 获取Spring的上下文对象
-    ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-
-    Demo demo = null;
-
-    log.info("获取 c-namespace 注入的 bean 对象 scope=\"singleton\" ");
-    for (int i = 0; i < 3; i++) {
-      // 获取 c-namespace 注入的 bean 对象 scope="singleton"
-      demo = applicationContext.getBean("beanDemo5", Demo.class);
-      log.info("beanDemo5 \n hashCode:" + demo.hashCode() + "\n" + demo.toString());
-    }
-
-    log.info("获取 c-namespace 注入的 bean 对象 scope=\"prototype\" ");
-    for (int i = 0; i < 3; i++) {
-      // 获取 c-namespace 注入的 bean 对象 scope="singleton"
-      demo = applicationContext.getBean("beanDemo6", Demo.class);
-      log.info("beanDemo6 \n hashCode:" + demo.hashCode() + "\n" + demo.toString());
-    }
-  }
-
-  @Test
-  public void testAutowire() {
-    // 获取Spring的上下文对象
-    ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-
-    Student student = null;
-
-    student = applicationContext.getBean("student1", Student.class);
-
-    log.info("获取 autowire=\"byName\" 注入的 bean 对象" + student);
+    Student student = applicationContext.getBean("student01", Student.class);
+    log.info("获取 p-namespace 注入的 bean 对象 beanDemo3 " + student.toString());
   }
 }
